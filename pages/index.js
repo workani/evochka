098100d8749b18cb4b91fee4340560d92bc6b783
danilminer234 <42529310+danilminer234@users.noreply.github.com>
2024-01-13 11:21:50 +0100

    updated pfp load times

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,33 @@
 import { ChakraProvider, Avatar, Box } from '@chakra-ui/react';
 import { FaInstagram, FaTelegramPlane, FaTiktok } from 'react-icons/fa';
-import * as React from 'react';
-import { useEffect } from 'react';
+import React from 'react';
 
 export default function Component() {
-  useEffect(() => {
-    // Автоматически прокручиваем страницу до самого низа при первой загрузке
-    window.scrollTo(0, document.documentElement.scrollHeight);
-  }, []);
+  const handleImageLoad = () => {
+    console.log('Image loaded successfully');
+  };
+
+  const handleImageError = () => {
+    console.error('Failed to load image');
+  };
 
   return (
     <ChakraProvider>
       <Box className="min-h-screen bg-black flex flex-col items-center justify-center">
         <Box className="p-6 space-y-6 bg-black rounded-lg flex flex-col items-center justify-center shadow-xl">
-          <Avatar size="3.5xl" src="/eva.png" className="w-48 h-48" />
+          <Avatar id="pfp-avatar" size="3.5xl" src="/eva.jpeg" className="w-48 h-48" alt="Profile picture of Eva" onLoad={handleImageLoad} onError={handleImageError} />
           <h1 className="text-center text-2xl font-semibold text-white">Hi, I'm Eva. Here's links to my social networks:</h1>
           
-          {/* Instagram button */}
           <a href="https://www.instagram.com/_y_e_v_a__/" target="_blank" className="w-full h-16 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-white text-xl font-bold rounded-lg shadow-lg flex items-center justify-center social-btn instagram">
             <FaInstagram className="text-white mr-2 text-3xl" />
             Instagram
           </a>
 
-          {/* Telegram button */}
           <a href="https://t.me/Inviseblee" target="_blank" className="w-full h-16 bg-gradient-to-r from-blue-500 to-light-blue-500 text-white text-xl font-bold rounded-lg shadow-lg flex items-center justify-center social-btn telegram">
             <FaTelegramPlane className="text-white mr-2 text-3xl" />
             Telegram
           </a>
           
-          {/* TikTok button with updated gradient */}
           <a href="https://www.tiktok.com/@evchanskiy_1?_t=8hO60oBaQLt&_r=1" target="_blank" className="w-full h-16 bg-gradient-to-r from-gray-900 via-black to-gray-800 text-white text-xl font-bold rounded-lg shadow-lg flex items-center justify-center social-btn tiktok">
             <FaTiktok className="text-white mr-2 text-3xl" />
             TikTok
